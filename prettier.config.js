e78156a434b6f7e32e3645e9bd552bca2439b0e9
Ken Vilar <kenvilar@gmail.com>
2025-10-08 14:30:40 +0800

    Remove Prettier configuration file and update HTML for cleaner formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,16 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-	plugins: ["prettier-plugin-tailwindcss"],
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+
+  // Keep things from wrapping aggressively
+  printWidth: 200,
+
+  // For HTML/Vue/etc., put the closing > on the last line if it wraps
+  bracketSameLine: true,
+
+  // Do NOT force one attribute per line
+  singleAttributePerLine: false,
+
+  // Be lenient about whitespace so Prettier is less eager to reflow
+  htmlWhitespaceSensitivity: "ignore",
+};
